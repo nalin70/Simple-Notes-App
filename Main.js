03ff959731408b184f4e876d36eb74713f043340
Nalin Dewangan <60232713+nalin70@users.noreply.github.com>
2022-09-23 10:07:25 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,35 @@
+
+
+const Main = ({ activeNote, onUpdateNote }) => {
+  const onEditField = (field, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [field]: value,
+    });
+  };
+
+  if (!activeNote) return <div className="no-active-note">No Active Note</div>;
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          id="title"
+          placeholder="Note Title"
+          value={activeNote.title}
+          onChange={(e) => onEditField("title", e.target.value)}
+          autoFocus
+        />
+        <textarea
+          id="body"
+          placeholder="Write your note here..."
+          value={activeNote.body}
+          onChange={(e) => onEditField("body", e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
